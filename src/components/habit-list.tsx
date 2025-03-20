6b1a793d599a03f851c0f9eb277eb7b453709892
Bobyson Laishram <acntanouba@gmail.com>
2025-03-20 23:20:27 +0530

    feat: streak

--- src/components/habit-list.tsx
+++ src/components/habit-list.tsx
@@ -1,5 +1,12 @@
-import { Box, Button, Grid, Paper, Typography } from "@mui/material";
-import useHabitStore from "../store/store";
+import {
+  Box,
+  Button,
+  Grid,
+  LinearProgress,
+  Paper,
+  Typography,
+} from "@mui/material";
+import useHabitStore, { Habit } from "../store/store";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import DeleteIcon from "@mui/icons-material/Delete";
 
@@ -7,6 +14,23 @@ const HabitList = () => {
   const { habits, removeHabit, toggleHabit } = useHabitStore();
 
   const today = new Date().toISOString().split("T")[0];
+
+  const getStreak = (habit: Habit) => {
+    let streak = 0;
+    const currentDate = new Date();
+
+    while (true) {
+      const dateString = currentDate.toISOString().split("T")[0];
+
+      if (habit.completedDates.includes(dateString)) {
+        streak++;
+        currentDate.setDate(currentDate.getDate() - 1);
+      } else {
+        break;
+      }
+    }
+    return streak;
+  };
 
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 3, mt: 4 }}>
@@ -44,6 +68,13 @@ const HabitList = () => {
               </Box>
             </Grid>
           </Grid>
+          <Box sx={{ mt: 2 }}>
+            <Typography>Current Streak: {getStreak(habit)}</Typography>
+            <LinearProgress
+              variant="determinate"
+              value={(getStreak(habit) / 30) * 100}
+            />
+          </Box>
         </Paper>
       ))}
     </Box>
